beec92b49687324b2f7482422be20a3c4f788fb8
philins <philins@ya.ru>
2021-01-16 20:01:54 +0300

    ready

--- isdark.py
+++ isdark.py
@@ -1,1 +1,22 @@
-import astral
+from astral import LocationInfo
+from astral.sun import sun
+from datetime import datetime, timedelta, date
+import pytz
+import subprocess
+
+
+city = LocationInfo(
+    name='Moscow',
+    region='Russian Federation',
+    timezone='Europe/Moscow',
+    latitude=55.75,
+    longitude=37.58)
+todaysDate = date.today();
+tz = pytz.timezone(city.timezone)
+currDate = datetime.now(tz)
+
+s = sun(city.observer, date=todaysDate, tzinfo=city.timezone)
+if s['sunrise'] + timedelta(hours = 1) < currDate < s['sunset'] - timedelta(hours = 1):
+    obj_retorno = subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "close", "02"],stdout=subprocess.PIPE , stderr=subprocess.PIPE)
+else:
+    obj_retorno = subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "open", "02"],stdout=subprocess.PIPE , stderr=subprocess.PIPE)
