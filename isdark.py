5f5f99bd82cd6d486172650ff814ebf1edb9fb4c
philins <philins@ya.ru>
2021-01-18 01:46:03 +0300

    Save status

--- isdark.py
+++ isdark.py
@@ -1,10 +1,11 @@
 from astral import LocationInfo
 from astral.sun import sun
 from datetime import datetime, timedelta, date
+import os
 import pytz
 import subprocess
 
-
+fName = "laststatus.log"
 city = LocationInfo(
     name='Moscow',
     region='Russian Federation',
@@ -15,8 +16,21 @@ todaysDate = date.today();
 tz = pytz.timezone(city.timezone)
 currDate = datetime.now(tz)
 
+if os.path.exists(fName):
+    with open(fName, "r") as f:
+        laststatus = f.read()
+else:
+    laststatus = "day"
+
 s = sun(city.observer, date=todaysDate, tzinfo=city.timezone)
 if s['sunrise'] + timedelta(hours = 1) < currDate < s['sunset'] - timedelta(hours = 1):
-    subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "close", "02"])
+    if laststatus == "night":
+        subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "close", "02"])
+    laststatus = "day"
 else:
-    subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "open", "02"])
+    if laststatus == "day":
+        subprocess.run(["CommandApp_USBRelay.exe", "BITFT", "open", "02"])
+    laststatus = "night"
+
+with open(fName, "w") as w:
+    w.write(laststatus)
